3c3934e1112784f9d510c58914b049c336ee0849
mohammadsajjadhossain9648 <sajjadhossain9648@gmail.com>
2024-10-19 16:49:18 +0600

    Initial project setup done

---
+++ tailwind_custom_configuration.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        white_color: 'rgb(255, 255, 255)',
+        shade_white_color1: 'rgb(231, 231, 231)',
+        shade_white_color2: 'rgb(243, 243, 243)',
+        footer_white_color: 'rgb(207, 207, 207)',
+
+        black_color: 'rgb(65, 64, 64)',
+
+        grey_color: 'rgb(112, 112, 112)',
+        shade_grey_color: 'rgb(160, 160, 160)',
+
+        brown_color: 'rgb(182, 140, 90)',
+      }
+    }
+  }
+}
